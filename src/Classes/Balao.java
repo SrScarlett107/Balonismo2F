4c0153f512aa2625903bc922b3de9ab90a0ef64b
SrScarlett107 <edu25070@gmail.com>
2023-06-06 08:26:11 -0300

    Atividade balão

--- src/Classes/Balao.java
+++ src/Classes/Balao.java
@@ -5,4 +5,15 @@ public class Balao {
     public int id;
 
     public String registro;
+
+    public String cor;
+    public String tipo;
+    public int capacidade;
+    double altura;
+    String porte;
+    double peso;
+
+    public String voar(){
+        return "voando";
+    }
 }
