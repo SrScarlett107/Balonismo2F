45766c6692fc97f835f9a7e7286926cecc49bd27
Matheus Nascimento <edu25070@gmail.com>
2023-06-13 08:31:11 -0300

    Atividade balão

---
+++ src/Classes/Pessoa.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Pessoa {
+    String nome;
+    String CPF;
+    String RG;
+    int telefone;
+    int emergencia;
+    String AssinaturaTermo;
+    String endereco;
+
+}
